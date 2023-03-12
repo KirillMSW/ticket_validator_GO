09a93e597886b244e43872d9a3f5d455905abda7
KirillMSW <kzybrow@yandex.ru>
2023-03-12 16:19:25 +0300

    added generate handler

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module ticket_validator_GO
 go 1.18
 
 require (
+	cloud.google.com/go/compute v1.18.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	github.com/bytedance/sonic v1.8.3 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/fogleman/gg v1.3.0 // indirect
@@ -13,6 +15,11 @@ require (
 	github.com/go-playground/validator/v10 v10.11.2 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
+	github.com/googleapis/gax-go/v2 v2.7.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.2 // indirect
@@ -23,12 +30,18 @@ require (
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.10 // indirect
+	go.opencensus.io v0.24.0 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.7.0 // indirect
 	golang.org/x/image v0.6.0 // indirect
 	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/oauth2 v0.6.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
+	google.golang.org/api v0.112.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20230303212802-e74f57abe488 // indirect
+	google.golang.org/grpc v1.53.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/fogleman/gg"
 	"github.com/gin-gonic/gin"
 	"github.com/skip2/go-qrcode"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
+	"google.golang.org/api/sheets/v4"
 	"image"
 	"image/draw"
 	"image/jpeg"
 	"io/ioutil"
+	"log"
 	"math/rand"
 	"net/http"
 	"os"
@@ -28,81 +34,45 @@ type Client struct {
 }
 
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+const spreadsheetId = "1XbvHbom9ErQKAmDgQOM39KCZsygF9p-IjGsiAYDS-Ik"
+
+var srv *sheets.Service
 
 func main() {
+	ctx := context.Background()
+
+	config := setConfig()
+	client := getClient(config)
+
+	srv = getService(ctx, client)
+
+	// Prints the names and majors of students in a sample spreadsheet:
+	// https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+	spreadsheetId := "1XbvHbom9ErQKAmDgQOM39KCZsygF9p-IjGsiAYDS-Ik"
+	readRange := "A:E"
+	resp, err := srv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve data from sheet: %v", err)
+	}
+
+	if len(resp.Values) == 0 {
+		fmt.Println("No data found.")
+	} else {
+		fmt.Println("Name, Major:")
+		for _, row := range resp.Values {
+			// Print columns A and E, which correspond to indices 0 and 4.
+			fmt.Printf("%s, %s\n", row[0], row[2])
+		}
+	}
+
+	fmt.Println("Begining app")
 	router := gin.Default()
 	router.POST("/api/generate", generate)
 	router.GET("/api/validate", validate)
 	router.Run("localhost:8080")
 }
 
-////// getAlbums responds with the list of all albums as JSON.
-////func getAlbums(c *gin.Context) {
-////	c.IndentedJSON(http.StatusOK, albums)
-////}
-////
-////// postAlbums adds an album from JSON received in the request body.
-////func postAlbums(c *gin.Context) {
-////	var newAlbum album
-////
-////	// Call BindJSON to bind the received JSON to
-////	// newAlbum.
-////	if err := c.BindJSON(&newAlbum); err != nil {
-////		return
-////	}
-////
-////	// Add the new album to the slice.
-////	albums = append(albums, newAlbum)
-////	c.IndentedJSON(http.StatusCreated, newAlbum)
-////}
-//
-//// getAlbumByID locates the album whose ID value matches the id
-//// parameter sent by the client, then returns that album as a response.
-//func getAlbumByID(c *gin.Context) {
-//	id := c.Param("id")
-//
-//	// Loop through the list of albums, looking for
-//	// an album whose ID value matches the parameter.
-//	for _, a := range albums {
-//		if a.ID == id {
-//			c.IndentedJSON(http.StatusOK, a)
-//			return
-//		}
-//	}
-//	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
-//}
-
-func generate(c *gin.Context) {
-	people_amount := 1
-	name := c.PostForm("name")
-	surname := c.PostForm("surname")
-	patronymic := c.PostForm("patronymic")
-	phone := c.PostForm("phone")
-	email := c.PostForm("email")
-
-	fmt.Println(people_amount, name, surname, patronymic, phone, email)
-	//var png []byte
-
-	jsonFile, err := os.Open("db.json")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("Successfully Opened users.json")
-	defer func(jsonFile *os.File) {
-		err := jsonFile.Close()
-		if err != nil {
-
-		}
-	}(jsonFile)
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-
-	var clients []Client
-	err = json.Unmarshal(byteValue, &clients)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("Successfully unmarshled users.json")
-
+func generateTicketId(clients []Client) string {
 	rand.Seed(time.Now().UnixNano())
 	generate_again := true
 	var ticket_id string
@@ -119,24 +89,28 @@ func generate(c *gin.Context) {
 			}
 		}
 	}
+	return ticket_id
+}
 
-	clients = append(clients, Client{
-		Ticket_id:     ticket_id,
-		People_amount: 1,
-		Name:          name,
-		Surname:       surname,
-		Patronymic:    patronymic,
-		Phone:         phone,
-		Email:         email,
-	})
-	result, err := json.Marshal(clients) //Returns the Json encoding of u into the variable result
+func pushToTables(ticket_id, surname, name, patronymic, phone, email string) {
+
+	readRange := "A:E"
+	resp, err := srv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
 	if err != nil {
-		fmt.Println("error", err)
+		log.Fatalf("Unable to retrieve data from sheet: %v", err)
 	}
-	err = os.WriteFile("db.json", result, 0644)
+	new_row_id := len(resp.Values) + 1
+	formatRange := fmt.Sprintf("A%d:G%d", new_row_id, new_row_id)
+	rb := &sheets.ValueRange{Values: [][]interface{}{{ticket_id, "АКТИВЕН", surname, name, patronymic, phone, email}}}
+
+	_, err = srv.Spreadsheets.Values.Update(spreadsheetId, formatRange, rb).ValueInputOption("USER_ENTERED").Do()
 	if err != nil {
 		fmt.Println(err)
 	}
+
+}
+
+func drawTicket(ticket_id, surname, name, patronymic string) {
 	png, err := qrcode.New(ticket_id, qrcode.Medium)
 	if err != nil {
 		fmt.Println(err)
@@ -184,6 +158,185 @@ func generate(c *gin.Context) {
 	if err != nil {
 		return
 	}
+}
+func getService(ctx context.Context, client *http.Client) *sheets.Service {
+	srv, err := sheets.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Sheets client: %v", err)
+	}
+	return srv
+}
+func setConfig() *oauth2.Config {
+	b, err := os.ReadFile("credentials.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	// If modifying these scopes, delete your previously saved token.json.
+	config, err := google.ConfigFromJSON(b, "https://www.googleapis.com/auth/spreadsheets")
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	return config
+}
+func getClient(config *oauth2.Config) *http.Client {
+	// The file token.json stores the user's access and refresh tokens, and is
+	// created automatically when the authorization flow completes for the first
+	// time.
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code: %v", err)
+	}
+
+	tok, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web: %v", err)
+	}
+	return tok
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+
+		}
+	}(f)
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+
+		}
+	}(f)
+	err = json.NewEncoder(f).Encode(token)
+	if err != nil {
+		return
+	}
+}
+
+////// getAlbums responds with the list of all albums as JSON.
+////func getAlbums(c *gin.Context) {
+////	c.IndentedJSON(http.StatusOK, albums)
+////}
+////
+////// postAlbums adds an album from JSON received in the request body.
+////func postAlbums(c *gin.Context) {
+////	var newAlbum album
+////
+////	// Call BindJSON to bind the received JSON to
+////	// newAlbum.
+////	if err := c.BindJSON(&newAlbum); err != nil {
+////		return
+////	}
+////
+////	// Add the new album to the slice.
+////	albums = append(albums, newAlbum)
+////	c.IndentedJSON(http.StatusCreated, newAlbum)
+////}
+//
+//// getAlbumByID locates the album whose ID value matches the id
+//// parameter sent by the client, then returns that album as a response.
+//func getAlbumByID(c *gin.Context) {
+//	id := c.Param("id")
+//
+//	// Loop through the list of albums, looking for
+//	// an album whose ID value matches the parameter.
+//	for _, a := range albums {
+//		if a.ID == id {
+//			c.IndentedJSON(http.StatusOK, a)
+//			return
+//		}
+//	}
+//	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+//}
+
+func generate(c *gin.Context) {
+
+	people_amount := 1
+	name := c.PostForm("name")
+	surname := c.PostForm("surname")
+	patronymic := c.PostForm("patronymic")
+	phone := c.PostForm("phone")
+	email := c.PostForm("email")
+
+	fmt.Println(people_amount, name, surname, patronymic, phone, email)
+	//var png []byte
+
+	jsonFile, err := os.Open("db.json")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Successfully Opened users.json")
+	defer func(jsonFile *os.File) {
+		err := jsonFile.Close()
+		if err != nil {
+
+		}
+	}(jsonFile)
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var clients []Client
+	err = json.Unmarshal(byteValue, &clients)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully unmarshled users.json")
+
+	ticket_id := generateTicketId(clients)
+	clients = append(clients, Client{
+		Ticket_id:     ticket_id,
+		People_amount: 1,
+		Name:          name,
+		Surname:       surname,
+		Patronymic:    patronymic,
+		Phone:         phone,
+		Email:         email,
+	})
+	result, err := json.Marshal(clients) //Returns the Json encoding of u into the variable result
+	if err != nil {
+		fmt.Println("error", err)
+	}
+	err = os.WriteFile("db.json", result, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	go pushToTables(ticket_id, surname, name, patronymic, phone, email)
+	drawTicket(ticket_id, surname, name, patronymic)
+
 	c.Header("Access-Control-Allow-Origin", "*")
 	c.File("new.jpg")
 
